203d38e160808970e27772fb09c884c44364bc64
Cyber_Infernox <sayonkar@gmail.com>
2023-11-16 11:16:29 +0530

    Onboarding page design
    Form components using shadcn
    Form implementation using zod resolver

--- my-app/components/shared/Topbar.tsx
+++ my-app/components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -30,7 +31,7 @@ const Topbar = () => {
         {/* OrgansationSwitcher is another thing from clerk */}
         <OrganizationSwitcher
           appearance={{
-            // baseTheme: dark,
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
