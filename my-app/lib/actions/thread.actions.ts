72e181b6c6e4b0b1da3b81823dbcc38e6f61606c
Cyber_Infernox <sayonkar@gmail.com>
2023-11-17 11:18:31 +0530

    Fetching threadposts
    Server side logic

--- my-app/lib/actions/thread.actions.ts
+++ my-app/lib/actions/thread.actions.ts
@@ -38,3 +38,46 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    // Calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts that have no parents (top-level threads...)
+    const postsQuery = await Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: {
+        $in: [null, undefined],
+      },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+
+    // // Revalidate also ensures that changes happen immediately on the website
+    // revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
+  }
+}
