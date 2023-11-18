c6387554f4a3af9fcf0abb6ae48b52f2c5985b47
Cyber_Infernox <sayonkar@gmail.com>
2023-11-18 10:52:31 +0530

    Thread card designed & posts fetched

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 // import { UserButton } from "@clerk/nextjs";
 
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
-// import ThreadCard from "@/components/cards/ThreadCard";
+import ThreadCard from "@/components/cards/ThreadCard";
 
 export default async function Home() {
   const user = await currentUser();
@@ -16,12 +14,13 @@ export default async function Home() {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchPosts(1, 30);
+  console.log(result);
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
 
-      {/* <section className="mt-9 flex flex-col gap-10">
+      <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
@@ -41,7 +40,7 @@ export default async function Home() {
             ))}
           </>
         )}
-      </section> */}
+      </section>
 
       {/* <UserButton afterSignOutUrl="/" /> */}
     </>
