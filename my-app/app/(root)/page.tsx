3f9d56056376b33284e7c62d17b7f34441759e34
Cyber_Infernox <sayonkar@gmail.com>
2023-11-18 12:03:54 +0530

    Single thread page init

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchPosts(1, 30);
-  console.log(result);
+  // console.log(result);
 
   return (
     <>
--- my-app/lib/actions/thread.actions.ts
+++ my-app/lib/actions/thread.actions.ts
@@ -78,3 +78,42 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 
   return { posts, isNext };
 }
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      // Multi-level commenting functionality
+      .populate({
+        path: "children", // Populate the children field
+        populate: [
+          {
+            path: "author", // Populate the author field within children
+            model: User,
+            select: "_id id name parentId image", // Select only _id and username fields of the author
+          },
+          {
+            path: "children", // Populate the children field within children
+            model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+            populate: {
+              path: "author", // Populate the author field within nested children
+              model: User,
+              select: "_id id name parentId image", // Select only _id and username fields of the author
+            },
+          },
+        ],
+      })
+      .exec(); // exec means execute
+
+    return thread;
+  } catch (error) {
+    console.error("Error while fetching thread:", error);
+    throw new Error("Unable to fetch thread");
+  }
+}
