51ca711aadae09ec5a9b7d93e8d29e952905f514
Cyber_Infernox <sayonkar@gmail.com>
2023-11-20 23:22:47 +0530

    Profile page designed

--- my-app/components/shared/LeftSidebar.tsx
+++ my-app/components/shared/LeftSidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 
 import { sidebarLinks } from "@/constants";
@@ -10,6 +10,8 @@ import { sidebarLinks } from "@/constants";
 const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -19,7 +21,7 @@ const LeftSidebar = () => {
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
 
-          // if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
             <Link
